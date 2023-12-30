e59ce0d7f3f86ea56eb44c769ea71d0e63be9262
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2023-12-30 13:44:38 +0700

    N

--- components/fetch.tsx
+++ components/fetch.tsx
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-async function fetchMain( props:any ) {
+async function fetchMain( __id:any ) {
     const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-    return response.data.find(({ id }):{ id:string } => ( id == props.id ))
+    return response.data.find(({ id }):{ id:string } => ( id == __id ))
 }
 
-export default async function Main({ props }:{ props:{ id:string } }) {
+export default async function Main( props:any ) {
     const mega = await fetchMain( props.id );
     return (
         <pre>{ JSON.stringify( mega, null, 4 ) }</pre>
