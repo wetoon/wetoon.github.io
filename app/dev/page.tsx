fc0f998006a57fe76d1d50ee8dfb317701916b41
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2023-12-30 13:53:58 +0700

    J

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -4,7 +4,8 @@ import Razer from "@component/fetch"
 import { useSearchParams } from "next/navigation"
 
 export default function Main() {
+    const params = useSearchParams();
     return (
-        <Razer id={ useSearchParams.get('id') } />
+        <Razer id={ params.get('id') } />
     )
 }
