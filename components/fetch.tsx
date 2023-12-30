af625d2f0d1ab3415b6db14b39a9d76c85a458d7
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2023-12-30 14:02:48 +0700

    Emit

--- components/fetch.tsx
+++ components/fetch.tsx
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 async function fetchMain( __id:any ) {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-    return response.data.find(({ id }:{ id:string }) => ( id == __id ))
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const metadata = response.data;
+    return __id ? metadata.find(({ id }:{ id:string }) => ( id == __id )) : metadata;
 }
 
 export default async function Main( props:any ) {
