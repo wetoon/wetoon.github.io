f0708dca5475590e2747b24472a2aa9be80de673
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2023-12-30 18:44:43 +0700

    H

--- components/fetch.tsx
+++ components/fetch.tsx
@@ -5,7 +5,7 @@ async function fetchMain( __id:any, query:any ) {
         const response = await axios.get( 'https://jsonplaceholder.typicode.com/' + query ? query : 'users' );
         const metadata = response.data;
         return __id ? metadata.find(({ id }:{ id:string }) => ( id == __id )) : metadata;
-    } catch (e) { return { code:404, message:"not found! } }
+    } catch (e) { return { code:404, message:"not found!" } }
 }
 
 export default async function Main( props:any ) {
