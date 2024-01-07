c84d266b3368b99e7299fb313c2a8d3cbc7f8bda
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-07 10:07:51 +0700

    Manifest

---
+++ app/manifest.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next"
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Wetoon',
+        short_name: 'Wetoon',
+        description: 'Wetoon Application',
+        start_url: '/',
+        display: 'fullscreen',
+        background_color: '#fff',
+        theme_color: '#fff',
+        icons: [{
+            src: '/favicon.png',
+            sizes: 'any',
+            type: 'image/png'
+        }],
+    }
+}
