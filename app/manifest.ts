24d6aabd396d591ffa88bc26dd9084111783f407
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-07 15:11:48 +0700

    Emit

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,37 +9,10 @@ export default function manifest(): MetadataRoute.Manifest {
         display: 'fullscreen',
         background_color: '#fff',
         theme_color: '#fff',
-        icons: [
-            {
-                src: '/favicon/android-chrome-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-            },
-            {
-                src: '/favicon/android-chrome-512x512.png',
-                sizes: '512x512',
-                type: 'image/png'
-            },
-            {
-                src: '/favicon/apple-touch-icon.png',
-                sizes: '180x180',
-                type: 'image/png'
-            },
-            {
-                src: '/favicon/favicon-16x16.png',
-                sizes: '16x16',
-                type: 'image/png'
-            },
-            {
-                src: '/favicon/favicon-32x32.png',
-                sizes: '32x32',
-                type: 'image/png'
-            },
-            {
-                src: '/favicon/mstile-150x150.png',
-                sizes: '150x150',
-                type: 'image/png'
-            }
-        ]
+        icons: [{
+            src: '/favicon.png',
+            sizes: 'any',
+            type: 'image/png'
+        }]
     }
 }
