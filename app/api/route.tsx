d6c1ca504b7b99bc4a22d8aca38f92972f788e1f
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-09 13:39:55 +0700

    GH

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,20 +1,16 @@
-
 import { NextResponse } from "next/server"
 import connect from "@lib/connect.database"
 import type { NextRequest } from "next/server"
-//import Collection from "@component/collection"
+
+export async function POST( request:NextRequest ) {
+    const NextRes = await request.json();
+    const message = await connect();
+    //const result = await Collection.member.find({});
+    return NextResponse.json( { message, NextRes } )
+}
 
 export async function GET() {
     const message = await connect();
     //const result = await Collection.member.find({});
     return NextResponse.json( { message } )
 }
-
-
-export async function POST( request:NextRequest ) {
-    await connect();
-    //const metadata = await request.json();
-    //await Collection.member.create( metadata );
-    return NextResponse.json( { metadata:"Created" } )
-}
-
