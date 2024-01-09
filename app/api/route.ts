8c11451dee86558a04ff3c962b6042990e712948
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-09 14:14:44 +0700

    TS

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server"
-import connect from "@lib/connect.database"
+//import connect from "@lib/connect.database"
 
-/*
+
 export async function POST( request:Request ) {
-    const message = await connect();
+    //const message = await connect();
     return NextResponse.json( { message } )
 }
-*/
+
 export async function GET( request:Request ) {
-    const message = await connect();
+    //const message = await connect();
     return NextResponse.json( { message } )
 }
