a8bd2469460f8d4f6033d3e136c7f6a00f833f83
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-09 12:09:32 +0700

    update route.tsx

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -6,12 +6,18 @@ import connect, { Collection } from "@lib/connect.database"
 export async function GET() {
     await connect();
     const result = await Collection.member.find({});
-    return NextResponse.json( result )
+    return NextResponse.json( result || { code:'404', message:'Data not found!' } )
 }
 
 export async function POST( request:NextRequest ) {
     await connect();
     const metadata = await request.json();
     await Collection.member.create( metadata );
-    return NextResponse.json( metadata )
+    return NextResponse.json( metadata, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
+    })
 }
