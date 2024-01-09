2f6d49766e11218c8951313d17b0a96f537ba71d
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-09 12:58:46 +0700

    RS

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -2,23 +2,17 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import connect from "@lib/connect.database"
-import Collection from "@component/collection"
+//import Collection from "@component/collection"
 
 export async function GET() {
-    await connect();
-    const result = await Collection.member.find({});
-    return NextResponse.json( result || { code:'404', message:'Data not found!' } )
+    const message = await connect();
+    //const result = await Collection.member.find({});
+    return NextResponse.json( { message } )
 }
 
 export async function POST( request:NextRequest ) {
-    await connect();
+    //await connect();
     const metadata = await request.json();
-    await Collection.member.create( metadata );
-    return NextResponse.json( metadata, {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        }
-    })
+    //await Collection.member.create( metadata );
+    return NextResponse.json( metadata )
 }
