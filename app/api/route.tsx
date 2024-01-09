9114414857a6e41e2295828271843fdf3204e474
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-09 13:46:17 +0700

    M

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -2,15 +2,12 @@ import { NextResponse } from "next/server"
 import connect from "@lib/connect.database"
 import type { NextRequest } from "next/server"
 
-export async function POST( request:NextRequest ) {
-    const NextRes = await request.json();
+export async function POST() {
     const message = await connect();
-    //const result = await Collection.member.find({});
-    return NextResponse.json( { message, NextRes } )
+    return NextResponse.json( { message } )
 }
 
 export async function GET() {
     const message = await connect();
-    //const result = await Collection.member.find({});
     return NextResponse.json( { message } )
 }
