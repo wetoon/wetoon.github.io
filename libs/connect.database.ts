45820728848279938634c223686b3814d85d5c65
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-09 10:52:49 +0700

    update connect.database.ts

--- libs/connect.database.ts
+++ libs/connect.database.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 
 export default async function connect() {
     try {
-        await mongoose.connect("mongodb+srv://admin:05478789za@wetoon.s5rnaaj.mongodb.net/wetoon").then(() => console.log('Connected!'));
+        await mongoose.connect("mongodb+srv://admin:05478789za@wetoon.s5rnaaj.mongodb.net/wetoon");
+        console.log('Connected!')
     } catch (e) { console.log(e) }
 }
