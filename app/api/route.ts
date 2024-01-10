2ab94bc279deec431c7f0432c295933bb778817a
6121280030+github@mtc.ac.th <6121280030+github@mtc.ac.th>
2024-01-10 08:00:23 +0700

    ns

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export async function GET( request:NextRequest ) {
+export function GET( request:NextRequest ) {
     return NextResponse.json( { code:200,message:"GET" } );
 }
 
-export async function POST( request:NextRequest ) {
+export function POST( request:NextRequest ) {
     return NextResponse.json( { code:200,message:"POST" } );
 }
