734bed41a2edb9e9a2e2cc6c03d572c8b1196e03
william <phiraphantanan@gmail.com>
2025-03-23 22:57:56 +0700

    fix homepage

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import { createRoot } from 'react-dom/client'
 
 import './tailwind.init.css'
+import './custom.css'
 
-import Portfolio from './components/Portfolio.tsx'
+import Home from './components/Home.tsx'
 import PageNotFound from './components/PageNotFound.tsx'
 
 createRoot( document.getElementById('app') as HTMLDivElement ).render(
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={ <Portfolio /> }></Route>
+            <Route path='/' element={ <Home /> }></Route>
             <Route path='*' element={ <PageNotFound /> }></Route>
         </Routes>
     </BrowserRouter>
